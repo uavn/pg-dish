ef2e3de362d38fad145453901e85a7b53541dd07
Artem Bondarenko <artbonvic@gmail.com>
2016-11-22 23:29:00 +0200

    asd

--- www/js/app.js
+++ www/js/app.js
@@ -10,28 +10,71 @@ var myApp = new Framework7({
     onAjaxStart: function (xhr) {
         myApp.showIndicator();
     },
+
     onAjaxComplete: function (xhr) {
         myApp.hideIndicator();
-    },
-
-    preprocess: function (content, url, next) {
-        if (url === 'people.html') {
-            var template = Template7.compile(content);
-            var resultContent = template({
-                title: 'People',
-                people: ['John', 'Ivan', 'Mary']
-            })
-            return resultContent;
-        }
     }
-});   
- 
-// If we need to use custom DOM library, let's save it to $$ variable:
-var $$ = Dom7;
- 
-// Add view
-var mainView = myApp.addView('.view-main', {
-  // Because we want to use dynamic navbar, we need to enable it for this view:
-  dynamicNavbar: true
 });
 
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+var app = {
+    init: function() {
+        $$('.tab-categories').click(function() {
+            $$('.art-tabs .tab-link').removeClass('active');
+            $$(this).addClass('active');
+
+            app.loadCategories();
+        });
+
+        $$('.tab-nations').click(function() {
+            $$('.art-tabs .tab-link').removeClass('active');
+            $$(this).addClass('active');
+
+            app.loadNations();
+        });
+
+        this.loadCategories();
+    },
+
+    loadCategories: function() {
+        myApp.showIndicator();
+
+        $$.ajax({
+            dataType: 'json',
+            url: 'http://r.uartema.com/api/api.php/category?transform=1&order=name',
+            success: function( resp ) {
+                var categoriesTemplate = $$('script#categories').html();
+                var compiledCategoriesTemplate = Template7.compile(categoriesTemplate);
+                $$('#categoriesContent').html(compiledCategoriesTemplate({categoies: resp.category}));
+
+                myApp.hideIndicator();
+            }
+        });
+    },
+
+    loadNations: function() {
+        myApp.showIndicator();
+
+        $$.ajax({
+            dataType: 'json',
+            url: 'http://r.uartema.com/api/api.php/nationality?transform=1&order=name',
+            success: function( resp ) {
+                var categoriesTemplate = $$('script#categories').html();
+                var compiledCategoriesTemplate = Template7.compile(categoriesTemplate);
+                $$('#categoriesContent').html(compiledCategoriesTemplate({categoies: resp.nationality}));
+
+                myApp.hideIndicator();
+            }
+        });
+    }
+};
+
+app.init();
